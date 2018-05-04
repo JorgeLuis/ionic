7a6551a1a661d539307da94edb0f96e140eb3007
JorgeLuis <lescanojorgeluis@gmail.com>
2018-05-04 19:02:56 -0300

    Sonidos

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { ANIMALES } from '../../assets/data/data.animales';
+import { Animal } from '../../interfaces/animales.interface';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  animales: Animal[] = [];
 
+  constructor() {
+    this.animales = ANIMALES.slice(0);//crea un nuevo valor
   }
 
+  reproducir(animal:Animal){
+    console.log(animal);
+    let audio = new Audio();
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+    animal.reproduciendo = true;
+
+    setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
+  }
 }
