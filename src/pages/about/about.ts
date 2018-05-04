5615b1f395c81ad703201978f2a2a635120ec02c
JorgeLuis <lescanojorgeluis@gmail.com>
2018-05-04 20:21:49 -0300

    Mejoras a los sonidos

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ANIMALES } from '../../assets/data/data.animales';
 import { Animal } from '../../interfaces/animales.interface';
+import { Refresher, reorderArray } from 'ionic-angular';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
@@ -8,20 +10,58 @@ import { Animal } from '../../interfaces/animales.interface';
 export class AboutPage {
 
   animales: Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando:boolean = false;
 
   constructor() {
     this.animales = ANIMALES.slice(0);//crea un nuevo valor
   }
 
-  reproducir(animal:Animal){
-    console.log(animal);
-    let audio = new Audio();
-    audio.src = animal.audio;
+  reproducir(animal: Animal) {
 
-    audio.load();
-    audio.play();
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+    console.log(animal);
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
     animal.reproduciendo = true;
 
-    setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
+    this.audioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
+  }
+
+  private pausar_audio(animalSelecionado: Animal) {
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSelecionado.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrar_animal(id: number) {
+    this.animales.splice(id, 1);
+  }
+
+  recargar_animales(refresher: Refresher) {
+    console.log('Inicio del refresh', refresher);
+
+    setTimeout(() => {
+      console.log('Termino el refresh');
+      this.animales = ANIMALES.slice(0);
+      refresher.complete();
+    }, 1500);
+  }
+
+  reordenar_animales(indices:any){
+      this.animales = reorderArray(this.animales, indices);
   }
 }
